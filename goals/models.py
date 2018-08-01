bb14a2391e9a0450f4e9be8d019ff42f7b72eecf
sudhanshu-jha <jha.sudhanshu1991@gmail.com>
2018-08-01 11:35:27 +0530

    add datefield in models

--- goals/models.py
+++ goals/models.py
@@ -7,6 +7,8 @@ class Goal(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=200)
     user = models.ForeignKey(User, null=True)
+    created=models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
