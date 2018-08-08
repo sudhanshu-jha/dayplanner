cf66d7362670db5b3cf829b7b96b0cc857ea088e
sudhanshu-jha <jha.sudhanshu1991@gmail.com>
2018-08-08 12:15:22 +0530

    password reset functionality

--- goals/urls.py
+++ goals/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
-
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    
     url(r'^$', views.index, name='index'),
     url(r'^add', views.add, name='add'),
     url(r'^edit/(?P<pk>\d+)/', views.edit, name='edit'),
@@ -11,6 +10,9 @@ urlpatterns = [
     url(r'^login', views.login_user, name='login'),
     url(r'^logout', views.logout_user, name='logout'),
     url(r'^signup',views.signup, name='signup'),
-     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate')
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',views.activate, name='activate'),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
--- goals/views.py
+++ goals/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 from goals.models import Goal
 from goals.forms import GoalForm, LoginForm, SignUpForm
 from django.contrib.auth.decorators import login_required
-
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
@@ -14,78 +13,60 @@ from .tokens import account_activation_token
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
 
-# Create your views here.
-
 
 @login_required
 def index(request):
-    if request.user.is_authenticated():
-        #show all the data irrespective of user
-        # goals = Goal.objects.all()[:40]
-        # show the data of a particular user
-        goals = Goal.objects.filter(user=request.user)
-        context = {
-            'goals': goals
-        }
-        return render(request, 'goals/index.html', context)
-    else:
-        return redirect('/login')
+    goals = Goal.objects.filter(user=request.user)
+    context = {
+        'goals': goals
+    }
+    return render(request, 'goals/index.html', context)
 
 @login_required
 def add(request):
-    if request.user.is_authenticated():
-        if request.method == 'POST':
-            form = GoalForm(request.POST)
-            #check whether form is valid
-            if form.is_valid():
-                #To create and save an object in a single step
-                Goal.objects.create(
-                    user=request.user,
-                    title=form.cleaned_data['title'],
-                    description=form.cleaned_data['description']
-                )
-                return redirect("/")       
-            else:
-                return redirect("")
+    if request.method == 'POST':
+        form = GoalForm(request.POST)
+        #check whether form is valid
+        if form.is_valid():
+            #To create and save an object in a single step
+            Goal.objects.create(
+                user=request.user,
+                title=form.cleaned_data['title'],
+                description=form.cleaned_data['description']
+            )
+            return redirect("/")       
         else:
-            # if a GET (or any other method) we'll create a blank form
-            form = GoalForm()
-            # it will create an empty form instance and place it in the template context/form to be rendered.
-        return render(request, 'goals/add.html', {'form': form})
+            return redirect("")
     else:
-        return redirect('/login')
+        # if a GET (or any other method) we'll create a blank form
+        form = GoalForm()
+        # it will create an empty form instance and place it in the template context/form to be rendered.
+    return render(request, 'goals/add.html', {'form': form})
 
 #############pk required#################
 
 @login_required
 def edit(request, pk):
-    if request.user.is_authenticated():
-        goal = Goal.objects.get(pk=pk,user=request.user)
-        if request.method == "POST":
-            form = GoalForm(request.POST)
-            #check whether form is valid
-            if form.is_valid():
-                # process the data in form.cleaned_data as required in is_valid()
-                user=request.user,
-                goal.title = form.cleaned_data['title']
-                goal.description = form.cleaned_data['description']
-                goal.save()
-                return redirect("/")
-        else:
-            # specify initial data for forms by specifying an initial parameter when instantiating the form.
-            form = GoalForm(initial={"title": goal.title,"description": goal.description})
-        return render(request, 'goals/add.html', {'form': form})
+    goal = Goal.objects.get(pk=pk,user=request.user)
+    if request.method == "POST":
+        form = GoalForm(request.POST)
+        #check whether form is valid
+        if form.is_valid():
+            # process the data in form.cleaned_data as required in is_valid()
+            user=request.user,
+            goal.title = form.cleaned_data['title']
+            goal.description = form.cleaned_data['description']
+            goal.save()
+            return redirect("/")
     else:
-         return redirect('/login')
+        # specify initial data for forms by specifying an initial parameter when instantiating the form.
+        form = GoalForm(initial={"title": goal.title,"description": goal.description})
+    return render(request, 'goals/add.html', {'form': form})
 
-
-
+@login_required
 def delete(request, pk):
-    if request.user.is_authenticated():
-        Goal.objects.get(pk=pk, user=request.user).delete()
-        return redirect("/")
-    else:
-        return HttpResponse("You are not authorized.")
+    Goal.objects.get(pk=pk, user=request.user).delete()
+    return redirect("/")
         # return redirect('/login')
 
 #############Accounts#################
@@ -117,29 +98,13 @@ def login_user(request):
             # if a GET (or any other method) we'll create a blank form
             form = LoginForm()
         # it will create an empty form instance and place it in the template context/form to be rendered.
-        return render(request, 'goals/login.html', {'form': form})
+        return render(request, 'registration/login.html', {'form': form})
     else:
         return redirect("/")
 
 def logout_user(request):
     logout(request)
     return redirect("/login")
-
-# def signup_user(request):
-#     if request.method == 'POST':
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             raw_password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=raw_password)
-#             login(request, user)
-#             return redirect('/')
-#     else:
-#         form = SignUpForm()
-#     return render(request, 'goals/signup.html', {'form': form})
-
-
 
 def signup(request):
     if request.method == 'POST':
@@ -148,7 +113,7 @@ def signup(request):
             user = form.save(commit=False)
             user.is_active = False
             user.save()
-            
+
             current_site = get_current_site(request)
             mail_subject = 'Activate your account.'
             message = render_to_string('goals/acc_active_email.html', {
@@ -163,7 +128,7 @@ def signup(request):
             return HttpResponse('Please confirm your email address to complete the registration')
     else:
         form = SignUpForm()
-    return render(request, 'goals/signup.html', {'form': form})
+    return render(request, 'registration/signup.html', {'form': form})
 
 
 def activate(request, uidb64, token):
@@ -176,7 +141,6 @@ def activate(request, uidb64, token):
         user.is_active = True
         user.save()
         login(request, user)
-        # return redirect('home')
         return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
     else:
         return HttpResponse('Activation link is invalid!')
@@ -197,6 +161,20 @@ def activate(request, uidb64, token):
 
 
 
+
+# def signup_user(request):
+#     if request.method == 'POST':
+#         form = SignUpForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(username=username, password=raw_password)
+#             login(request, user)
+#             return redirect('/')
+#     else:
+#         form = SignUpForm()
+#     return render(request, 'goals/signup.html', {'form': form})
 
 
 
