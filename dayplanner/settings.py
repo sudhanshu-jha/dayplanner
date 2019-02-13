87ac036d939ee1690be1d2c08b4ebd02cff2443f
sudhanshu-jha <jha.sudhanshu1991@gmail.com>
2019-02-13 22:45:28 +0530

    Dockerize project

--- dayplanner/settings.py
+++ dayplanner/settings.py
@@ -82,12 +82,14 @@ WSGI_APPLICATION = "dayplanner.wsgi.application"
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
